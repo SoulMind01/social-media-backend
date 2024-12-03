904e05d0b6e20e3839cbf0f0b1c76abf04cfda62
SoulMind <jackyrobot01@gmail.com>
2024-12-02 16:48:38 -0800

    updated `vpc.tf`

--- vpc.tf
+++ vpc.tf
@@ -27,3 +27,83 @@ resource "aws_internet_gateway" "app_gw" {
     Name = "App-Internet-Gateway"
   }
 }
+
+resource "aws_security_group" "bastion_sg" {
+  name        = "bastion-sg"
+  description = "Allow SSH access to the bastion host"
+  vpc_id      = aws_vpc.app_vpc.id
+
+  ingress {
+    description = "SSH from anywhere"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Restrict this to your IP for better security
+  }
+
+  egress {
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "BastionSecurityGroup"
+  }
+}
+
+resource "aws_security_group" "elasticache_access_sg" {
+  name        = "elasticache-access-sg"
+  description = "Allow access to ElastiCache from bastion"
+  vpc_id      = aws_vpc.app_vpc.id
+
+  ingress {
+    description     = "Allow Redis traffic from Bastion"
+    from_port       = 6379
+    to_port         = 6379
+    protocol        = "tcp"
+    security_groups = [aws_security_group.bastion_sg.id]
+  }
+
+  egress {
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ElastiCacheAccessSecurityGroup"
+  }
+}
+# Public Route Table
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.app_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.app_gw.id
+  }
+
+  tags = {
+    Name = "PublicRouteTable"
+  }
+}
+
+# Associate Subnet with Public Route Table
+resource "aws_route_table_association" "public_subnet_association" {
+  subnet_id      = aws_subnet.app_subnet.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+resource "aws_elasticache_subnet_group" "app_subnet_group" {
+  name        = "app-cache-subnet-group"
+  description = "Subnet group for App ElastiCache"
+  subnet_ids  = [aws_subnet.app_subnet.id] # Subnet from your custom VPC
+
+  tags = {
+    Name = "AppCacheSubnetGroup"
+  }
+}
